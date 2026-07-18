c6fd1c58f19e5bdd56cd5ac16995c6244ef78ba5
Aman Raj <amanofficial3255@gmail.com>
2026-07-18 14:15:25 +0530

    Week 1: Exercise 2 - Factory Method Pattern

---
+++ Week1-EngineeringConcepts/DesignPatterns/factory/DocumentFactory.java
@@ -0,0 +1,11 @@
+package factory;
+
+public abstract class DocumentFactory {
+
+    public abstract Document createDocument();
+
+    public void openDocument() {
+        Document document = createDocument();
+        document.open();
+    }
+}
