8ce5dcb53c651d27b2a08478a432e41a8c74c384
Aman Raj <amanofficial3255@gmail.com>
2026-07-22 21:56:55 +0530

    Added React StudentApp Hands-on Lab

---
+++ Week5-React/studentapp/src/Components/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class About extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Welcome to the About page of the Student Management Portal</h2>
+            </div>
+        );
+    }
+}
+
+export default About;
