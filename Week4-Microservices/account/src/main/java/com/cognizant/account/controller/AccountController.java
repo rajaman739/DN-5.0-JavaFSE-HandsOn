1ab56fb87f5a165e2e25a27f6e5bea3abd506688
Aman Raj <amanofficial3255@gmail.com>
2026-07-22 17:52:38 +0530

    Microservices: Account and Loan services running on separate ports

---
+++ Week4-Microservices/account/src/main/java/com/cognizant/account/controller/AccountController.java
@@ -0,0 +1,29 @@
+package com.cognizant.account.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestController
+public class AccountController {
+
+    private static final Logger logger = LoggerFactory.getLogger(AccountController.class);
+
+    @GetMapping("/accounts/{number}")
+    public Map<String, Object> getAccount(@PathVariable String number) {
+        logger.info("Start: getAccount() invoked for account number: {}", number);
+
+        Map<String, Object> account = new LinkedHashMap<>();
+        account.put("number", number);
+        account.put("type", "savings");
+        account.put("balance", 234343);
+
+        logger.info("End: getAccount() completed for account number: {}", number);
+        return account;
+    }
+}
